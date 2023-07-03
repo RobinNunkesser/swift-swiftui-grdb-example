2e954c647feb00b105c3695655dc54e427b6cf42
nunkesser <robin.nunkesser@hshl.de>
2023-07-03 20:51:19 +0200

    Initial Commit

---
+++ GRDBExample/GRDBExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRDBExampleApp.swift
+//  GRDBExample
+//
+//  Created by Prof. Dr. Nunkesser, Robin on 03.07.23.
+//
+
+import SwiftUI
+
+@main
+struct GRDBExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
